2bb8e2ecae18df00ae6da63c7adc4baa6b5c7ccc
Žan Pevec <pevecyan@outlook.com>
2017-11-20 16:48:35 +0100

    Added exclude list

--- README.md
+++ README.md
@@ -12,29 +12,29 @@ Find free tcp port or ports to listen locally.
 Find a free port to listen on, begin from 3000
 
     var fp = require("find-free-port")
-    fp(3000, function(err, freePort){
+    fp(3000,[], function(err, freePort){
     });
     
 Find a free port to listen on, begin from 3000, but stop at 3100 (not including 3100)
 
     var fp = require("find-free-port")
-    fp(3000, 3100, function(err, freePort){
+    fp(3000,[], 3100, function(err, freePort){
     });
     
 Find a free port to listen on, begin from 3000, stop at 3100, bound to ip 127.0.0.1 only
 
     var fp = require("find-free-port")
-    fp(3000, 3100, '127.0.0.1', function(err, freePort){
+    fp(3000,[], 3100, '127.0.0.1', function(err, freePort){
     });
     
 Find a free port to listen on, begin from 3000, bound to ip 127.0.0.1 only
 
     var fp = require("find-free-port")
-    fp(3000, '127.0.0.1', function(err, freePort){
+    fp(3000,[], '127.0.0.1', function(err, freePort){
     });
     
 Find a few free ports to listen on, begin from 3000, bound to ip 127.0.0.1 only
 
     var fp = require("find-free-port")
-    fp(3000, 3100, '127.0.0.1', 3, function(err, p1, p2, p3){
+    fp(3000,[], 3100, '127.0.0.1', 3, function(err, p1, p2, p3){
     });
--- index.js
+++ index.js
@@ -1,45 +1,57 @@
 const net = require("net");
 
-// call method 1: (port, cb(err, freePort))
-// call method 2: (portBeg, portEnd, cb(err, freePort))
-// call method 3: (portBeg, host, cb(err, freePort))
-// call method 4: (portBeg, portEnd, host, cb(err, freePort))
-// call method 5: (portBeg, portEnd, host, howmany, cb(err, freePort1, freePort2, ...))
+// call method 1: (port, exclude, cb(err, freePort))
+// call method 2: (portBeg, exclude, portEnd, cb(err, freePort))
+// call method 3: (portBeg, exclude, host, cb(err, freePort))
+// call method 4: (portBeg, exclude, portEnd, host, cb(err, freePort))
+// call method 5: (portBeg, exclude, portEnd, host, howmany, cb(err, freePort1, freePort2, ...))
 
-module.exports = function(beg, ...rest){
-  const p = rest.slice(0, rest.length - 1), cb = rest[rest.length - 1];
-  let [end, ip, cnt] = Array.from(p);
-  if (!ip && end && !/^\d+$/.test(end)) { // deal with method 3
-    ip = end;
-    end = 65534;
-  } else {
-    if (end == null) { end = 65534; }
-    if (ip == null) { ip = '0.0.0.0'; }
-  }
-  if (cnt == null) { cnt = 1; }
-
-  const retcb = cb;
-  const res = [];
-  const prob = function(ip, port, cb){
-    const s = net.createServer().listen(port, ip);
-    s.on('listening', function(){ s.close(); return cb(port); });
-    return s.on('error', err=> cb(null, port + 1));
-  };
-  var onprob = function(port, nextPort){
-    if (port) {
-      res.push(port);
-      if (res.length >= cnt) {
-        return retcb.apply(null, [null].concat(res));
+module.exports = {
+    find:function(beg, exclude, ...rest){
+      const p = rest.slice(0, rest.length - 1), cb = rest[rest.length - 1];
+      let [end, ip, cnt] = Array.from(p);
+      if (!ip && end && !/^\d+$/.test(end)) { // deal with method 3
+        ip = end;
+        end = 65534;
       } else {
-        return setImmediate(()=> prob(ip, port+1, onprob));
+        if (end == null) { end = 65534; }
+        if (ip == null) { ip = '0.0.0.0'; }
       }
-    } else {
-      if (nextPort>=end) {
-        return retcb(new Error("No available ports"));
-      } else {
-        return setImmediate(()=> prob(ip, nextPort, onprob));
-      }
+      if (cnt == null) { cnt = 1; }
+    
+      const retcb = cb;
+      const res = [];
+      const prob = function(ip, port, cb){
+        if(exclude.indexOf(port)>-1) return cb(null, port+1);
+        const s = net.createServer().listen(port, ip);
+        s.on('listening', function(){ s.close(); return cb(port); });
+        return s.on('error', err=> cb(null, port + 1));
+      };
+      var onprob = function(port, nextPort){
+        if (port) {
+          res.push(port);
+          if (res.length >= cnt) {
+            return retcb.apply(null, [null].concat(res));
+          } else {
+            return setImmediate(()=> prob(ip, port+1, onprob));
+          }
+        } else {
+          if (nextPort>=end) {
+            return retcb(new Error("No available ports"));
+          } else {
+            return setImmediate(()=> prob(ip, nextPort, onprob));
+          }
+        }
+      };
+      return prob(ip, beg, onprob);
+    },
+    test: function(port, exclude){
+      return new Promise(function(resolve, reject){
+        if(exclude.indexOf(port) > -1) return reject();
+        const s = net.createServer().listen(port, ip);
+        s.on('listening', function(){ s.close(); return resolve(port)});
+        return s.on('error', err=> reject(port));
+      });
     }
-  };
-  return prob(ip, beg, onprob);
-};
+}
+
